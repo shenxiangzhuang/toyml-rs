a523c24255a2603685e51f280853d29114962564
Xiangzhuang Shen <datahonor@gmail.com>
2024-12-03 18:18:34 +0800

    fix

--- examples/kmeans.py
+++ examples/kmeans.py
@@ -2,7 +2,7 @@ from toymlrs import KmeansRust
 
 
 def run():
-    xs = [[0], [0], [10], [10]]
+    xs = [[0.0], [0.0], [10.0], [10.0]]
     kmeans = KmeansRust(k=2, max_iter=100)
     print(kmeans.fit_predict(xs))
 

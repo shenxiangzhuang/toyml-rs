aef32466f662654cfd163ed5eab4036a88f5c6dc
Xiangzhuang Shen <datahonor@gmail.com>
2024-12-06 13:20:41 +0800

    improve(kmeans): add more property in python

--- bindings/python/examples/kmeans.py
+++ bindings/python/examples/kmeans.py
@@ -5,6 +5,8 @@ def run():
     xs = [[0.0], [0.0], [10.0], [10.0]]
     kmeans = Kmeans(k=2, max_iter=100, centroids_init_method="kmeans++", distance_metric="euclidean", random_seed=42)
     print(kmeans.fit_predict(xs))
+    print(kmeans.centroids_)
+    print(kmeans.clusters_)
 
 
 if __name__ == '__main__':
--- bindings/python/src/clustering/kmeans.rs
+++ bindings/python/src/clustering/kmeans.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 #[pyclass]
@@ -17,7 +18,7 @@ impl Kmeans {
             self.inner.distance_metric,
         )
     }
-    
+
     #[new]
     #[pyo3(signature = (k, max_iter=100, centroids_init_method="random", distance_metric="euclidean", random_seed=None))]
     fn py_new(
@@ -46,13 +47,35 @@ impl Kmeans {
         self.inner.fit(point_values);
     }
 
-    pub fn fit_predict(&mut self, point_values: Vec<Vec<f64>>) -> PyResult<Vec<usize>> {
+    pub fn fit_predict(&mut self, point_values: Vec<Vec<f64>>) -> PyResult<&Vec<usize>> {
         self.fit(point_values);
-        self.labels()
+        self.labels_()
     }
 
     #[getter]
-    pub fn labels(&self) -> PyResult<Vec<usize>> {
-        Ok(self.inner.get_labels().0.clone())
+    pub fn labels_(&self) -> PyResult<&Vec<usize>> {
+        Ok(&self.inner.get_labels().0)
+    }
+
+    #[getter]
+    pub fn centroids_(&self) -> HashMap<usize, &Vec<f64>> {
+        HashMap::from_iter(
+            self.inner
+                .get_centroids()
+                .centroid_map
+                .iter()
+                .map(|(k, v)| (*k, &v.values)),
+        )
+    }
+
+    #[getter]
+    pub fn clusters_(&self) -> HashMap<usize, &Vec<usize>> {
+        HashMap::from_iter(
+            self.inner
+                .get_clusters()
+                .cluster_map
+                .iter()
+                .map(|(k, v)| (*k, &v.point_indices)),
+        )
     }
 }

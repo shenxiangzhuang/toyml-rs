20f8f934c1acc8796d8925eda35fa5938e5cb402
Xiangzhuang Shen <datahonor@gmail.com>
2024-12-09 13:46:05 +0800

    test: cargo pass; js fail

--- bindings/js/index.js
+++ bindings/js/index.js
@@ -1,4 +1,8 @@
-import {greet, test_kmeans} from './pkg';
+import {greet, Kmeans, KmeansOptions} from './pkg';
 
-// greet('World');
-test_kmeans()
+greet('World');
+
+let opts: KmeansOptions = {k: 2, maxIter: 100, randomSeed: 42}
+let kmeans: Kmeans = {opts: opts};
+let labels = kmeans.fit_predict([[0], [0], [1], [1]])
+console.log(labels)

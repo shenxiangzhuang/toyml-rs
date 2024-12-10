793dd5ee64cb331349db0982cdbf84d7b94f67c7
Xiangzhuang Shen <datahonor@gmail.com>
2024-12-10 18:17:39 +0800

    change to webpack: pass

--- bindings/js/example/webpack.config.js
+++ bindings/js/example/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
@@ -8,17 +7,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-        },
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin(),
@@ -29,8 +17,5 @@ module.exports = {
   mode: "development",
   experiments: {
     asyncWebAssembly: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
 };
